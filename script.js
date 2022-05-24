50647783600862264eff0be3c56095c115aa0ada
Fortwin <basirathamz@gmail.com>
2022-05-24 07:27:41 +0100

    added new insertion

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function createBoard() {
    const card = document.createElement('img')
    card.setAttribute('src', 'images/blank.jpg')
    card.setAttribute('data-id', i)
-   console.log(card , i)
+   gridDisplay.appendChild(card)
   }
 }
 
